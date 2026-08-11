946919efa62b3c647dd883d100dc4cb6a1afb3cd
Aayushi Shukla <aayushishukla27@gmail.com>
2026-08-10 21:45:30 -0400

    Layout / landing page - Savepoint #1

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,9 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    NgClass
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {}
+export class Layout {
+
+  isSidebarOpen: boolean = true;
+
+}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  loginObject: any = {
+    userName: '',
+    password: ''
+  };
+
+  router = inject(Router)
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  onLogin() {
+    this.http.post(environment.API_URL + 'login', this.loginObject)
+    .subscribe({
+      next: (response: any) => {
+        if(response.result) {
+          alert("User logged in successfully");
+          this.router.navigateByUrl("/admin/dashboard");
+        }
+        else {
+          alert(response.message);
+        }
+      },
+      error: (error: any) => {
+        alert("An error occurred while logging in");
+      }
+    })
+  }
+}
